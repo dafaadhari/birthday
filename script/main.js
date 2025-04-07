d1642e4e17a317633c7edc5e884fcdaf4de00370
dapa adhari <dafaadhari01@gmail.com>
2025-04-07 19:56:55 +0700

    fix

--- script/main.js
+++ script/main.js
@@ -118,13 +118,14 @@ const animationTimeline = () => {
     .to(".idea-1", 0.7, ideaTextTransLeave, "+=2.5")
     .from(".idea-2", 0.7, ideaTextTrans)
     .to(".idea-2", 0.7, ideaTextTransLeave, "+=2.5")
-    .from(".idea-3", 0.7, ideaTextTrans)
-    .to(".idea-3 strong", 0.5, {
+    .from(".idea-3", 1, ideaTextTrans)
+    .to(".idea-3 strong", 0.4, {
         scale: 1.2,
         x: 10,
         backgroundColor: "rgb(21, 161, 237)",
         color: "#fff",
     })
+    .to({}, 2, {})
     .to(".idea-3", 0.7, ideaTextTransLeave, "+=2.5")
     .from(".idea-4", 0.7, ideaTextTrans)
     .to(".idea-4", 0.7, ideaTextTransLeave, "+=2.5")
